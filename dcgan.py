e180f4ead86ee8ccab9c1bcfd827781a61029fa9
sghan <sungguk80@gmail.com>
2018-01-16 02:30:24 +0900

    keras 2.0

--- dcgan.py
+++ dcgan.py
@@ -18,7 +18,7 @@ from keras.layers.core import Flatten
 from keras.optimizers import SGD, Adam
 from keras.datasets import mnist
 from keras.utils import np_utils
-from keras import initializations
+#from keras import initializations
 
 def dataInit():
 	print('Loading the data')
@@ -48,10 +48,6 @@ def saveImage(imageData, imageName, epoch):
 	f.savefig('images/'+imageName+'_after_'+str(epoch)+'_epoch.png', dpi = 100, bbox_inches='tight', pad_inches = 0)
 	plt.close(f)
 	return None
-
-def initNormal(shape, name=None):
-	return initializations.normal(shape, scale=0.02, name=name)
-
 if __name__ == '__main__':
 
 	batchSize = 128
@@ -73,7 +69,7 @@ if __name__ == '__main__':
 	print('Generator Model')
 
 	generator = Sequential()
-	generator.add(Dense( input_dim=100, output_dim=(128*7*7), init=initNormal))
+	generator.add(Dense( input_dim=100, output_dim=(128*7*7))) #initialization
 	generator.add(Activation('relu'))
 	generator.add(Reshape((128, 7, 7)))	
 	generator.add(UpSampling2D(size=(2, 2)))
@@ -87,7 +83,7 @@ if __name__ == '__main__':
 	print('Discriminator Model')
 
 	discriminator = Sequential()
-	discriminator.add(Convolution2D(64, 5, 5, border_mode='same', subsample=(2,2), input_shape=(1,28,28), init=initNormal))
+	discriminator.add(Convolution2D(64, 5, 5, border_mode='same', subsample=(2,2), input_shape=(1,28,28))) #initialization 
 	discriminator.add(LeakyReLU(0.2))
 	discriminator.add(Convolution2D(128, 5, 5, border_mode='same', subsample=(2,2)))
 	discriminator.add(LeakyReLU(0.2))
